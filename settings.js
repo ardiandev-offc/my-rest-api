751070bfc750911673cb02cf796129fb4c82ec02
ArdianStore <vylovemdbot@gmail.com>
2024-06-29 18:27:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'ArdianOfc'// yourname
+global.MONGO_DB_URI = "mongodb+srv://vylovemdbot:DhtHw9iPn0VeTpRq@cluster0.b2swn5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "no-reply@topupkuy.xyz" //email
+global.email_password = "OT6weK@8(N5qq7" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
